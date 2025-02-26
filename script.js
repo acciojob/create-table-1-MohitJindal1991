2c22fd24f3fe91570f2ab9a51aef8402f23abe33
MohitJindal1991 <90623594+MohitJindal1991@users.noreply.github.com>
2025-02-26 14:57:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
+const table=document.querySelector("#sampleTable");
 function insert_Row() {
     //Write your code here
+	const newElement=document.createElement("tr");
+	newElement.innerHTML=`<td>New Cell1</td><td>New Cell2</td>`
+	table.prepend(newElement);
   
   
 }
