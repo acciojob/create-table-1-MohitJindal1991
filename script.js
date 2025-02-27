05b7180a98e8275c31658462b038be45e2eced03
MohitJindal1991 <90623594+MohitJindal1991@users.noreply.github.com>
2025-02-27 22:18:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 const table=document.querySelector("#sampleTable");
 function insert_Row() {
-    //Write your code here
-	const newElement=document.createElement("tr");
-	newElement.innerHTML=`<td>New Cell1</td><td>New Cell2</td>`
-	table.prepend(newElement);
-  
-  
+    const newRow = table.insertRow();
+    const cell1 = newRow.insertCell(0);
+    const cell2 = newRow.insertCell(1);
+    cell1.innerHTML = 'New Cell1';
+    cell2.innerHTML = 'New Cell2';
 }
